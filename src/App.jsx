5482f8e9543f635c9cfcebe5585be0662468f7b9
Will Lumby <wlumby@gmail.com>
2021-02-26 13:38:22 +0000

    Make page responsive and work on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -46,15 +46,24 @@ class App extends React.Component {
         <div className="Main-content">
           <ArcherContainer strokeColor="white">
             <div className="Chart-row">
+              <ChartNode id='mechanar' nodeText='The Mechanar'/>
+              <ChartNode id='botanica' nodeText='The Botanica'/>
+              <ChartNode id='steam-vaults' nodeText='The Steam Vaults'/>
+              <ChartNode id='durnholde' nodeText='Durnholde'/>
+            </div>
+            
+            <div className="Chart-row">
               <ChartNode id='kazzak' nodeText='Kazzak'/>
-              <ChartNode id='arcatraz-questline' nodeText='Arcatraz Questline'/>
+              <ChartNode id='arcatraz-questline' relations={this.generateRelations('mechanar', 'bottom', 'top', 'botanica', 'bottom', 'top')} nodeText='Arcatraz Questline'/>
               <ChartNode id='magtheridon' nodeText='Magtheridon'/>
+              <ChartNode id='shadow-labs' relations={this.generateRelations('steam-vaults', 'bottom', 'top')} nodeText='Shadow Labs'/>
+              <ChartNode id='cot-entrance-quest' relations={this.generateRelations('durnholde', 'bottom', 'top')} nodeText='CoT Entrance Quest'/>
             </div>
 
             <div className="Chart-row">
               <ChartNode id='flying-mount' relations={this.generateRelations('kazzak', 'bottom', 'top', 'arcatraz-questline', 'bottom', 'top')} nodeText='Flying Mount'/>
               <ChartNode id='level-70' relations={this.generateRelations('flying-mount', 'right', 'left', 'magtheridon', 'bottom', 'top', 'start-karazhan-quest', 'left', 'right')} nodeText='Level 70'/>
-              <ChartNode id='start-karazhan-quest' nodeText='Start Karazhan Quest'/>
+              <ChartNode id='start-karazhan-quest' relations={this.generateRelations('shadow-labs', 'bottom', 'top')} nodeText='Start Karazhan Quest'/>
           </div>
           </ArcherContainer>
         </div>
