87056eb276ee36a654cc915027de9c6066749145
Will Lumby <wlumby@gmail.com>
2021-03-05 15:23:28 +0000

    Add relation clicking

--- src/ChartNode.jsx
+++ src/ChartNode.jsx
@@ -4,16 +4,35 @@ import { ArcherElement } from 'react-archer';
 import './ChartNode.css';
 
 class ChartNode extends React.Component {
+    
+    relations = [];
+    
     render = () => {
         return (
             <ArcherElement id={this.props.id} relations={this.props.relations}>
-                <span className='ChartNode'>
+                <span onClick={this.click} id={`${this.props.id}-text`}className='ChartNode'>
                     <span className='ChartNodeText'>
                         {this.props.nodeText}
                     </span>
                 </span>
             </ArcherElement>
         )
+    }
+
+    componentDidMount = () => {
+        if (this.props.relations) {
+            this.props.relations.forEach((relation) => {
+                this.relations.push(relation.targetId + '-text')
+                document.getElementById(relation.targetId + '-text').style = "background-color:grey;"
+            })
+        }
+    }
+
+    click = () => {
+        this.relations.forEach((relation) => {
+            document.getElementById(relation).style = "background-color:rgb(72, 190, 240);"
+        })
+        document.getElementById(this.props.id + '-text').style = "background-color:lightgreen";
     }
 }
 
